08d28ccddb406d7aff9a6fadb3fb029fa2c883c0
Daniel Willemse <daniel.willemse@bettyblocks.com>
2018-06-20 00:33:51 +0200

    Add Room channel

--- lib/chatly_web/channels/user_socket.ex
+++ lib/chatly_web/channels/user_socket.ex
@@ -2,7 +2,7 @@ defmodule ChatlyWeb.UserSocket do
   use Phoenix.Socket
 
   ## Channels
-  # channel "room:*", ChatlyWeb.RoomChannel
+  channel "room:*", ChatlyWeb.RoomChannel
 
   ## Transports
   transport :websocket, Phoenix.Transports.WebSocket
